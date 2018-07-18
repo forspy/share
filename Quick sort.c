9277b5cfe9e2ee96b85f914bbfad8beb98ea5573
forspy <qiubow@126.com>
2018-07-18 14:07:27 +0800

    快速排序_与中位数比_双指针

---
+++ Quick sort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void qsort(int v[], int left, int right);//从指定位置开始快速排序
+main()
+{
+	int a[9] = { 4,5,8,7,6,9,2,1,3 };
+	int left = 0;
+	int right = 8;
+	int i;
+	qsort(a, left, right);
+	for (i = 0; i <= 8; i++)
+		printf("%d", a[i]);
+	return 0;
+}
+
+void qsort(int v[], int left, int right)
+{
+	int i, last;
+	void swap(int v[], int i, int j);
+	if (left >= right)//两个要排，一个就不用排了
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left + 1; i <= right; i++)
+	{
+		if (v[i]< v[left])
+			swap(v, ++last, i);
+	}
+	swap(v, left, last);
+	qsort(v, left, last - 1);
+	qsort(v, last + 1, right);
+}
+
+void swap(int v[], int i, int j)
+{
+	int temp;
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
