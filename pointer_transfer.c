89632652ed8572bfcffa0068a44951c3a396071c
forspy <qiubow@126.com>
2018-07-20 15:31:46 +0800

    指针赋值运算（与数组赋值比较，本质是一样的）进阶写法

--- pointer_transfer.c
+++ pointer_transfer.c
@@ -2,13 +2,14 @@
 
 void strcpy1(char *s, char *t);
 void strcpy(char *s, char *t);
+void strcpy2(char *s, char *t);
 int main()
 {
 	char s[] = "hello";
 	char t[] = "hello world";
-	strcpy1(s, t);//1.在char t[] = "world!";char s[] = "hello world!";的时候，显示world，即短内存付给长内存
+	strcpy2(s, t);//1.在char t[] = "world!";char s[] = "hello world!";的时候，显示world，即短内存付给长内存
+	//当长内存赋值给短内存的时候，短内存字符串长度不够，指针移到尾部后不能再移动
 	printf("%s", s);
-	
 }
 
 void strcpy1(char *s, char *t)
@@ -27,4 +28,9 @@ void strcpy(char *s, char *t)
 		t++;
 		
 	}
+}
+
+void strcpy2(char*s, char *t)
+{
+	while (*s++, *t++);
 }
