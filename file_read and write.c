ab85622bc627f1983a7ef4b1cffebf251d237282
forspy <qiubow@126.com>
2018-07-31 17:46:12 +0800

    文件读写1

---
+++ file_read and write.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+main()
+{
+	FILE *fid = fopen("out.txt", "r");
+	if (fid == NULL)
+	{
+		printf("打开%s失败\n", "txt_out.txt");//
+		return;
+	}
+	int mode = 2;
+if (mode == 1)
+{
+	//按字符读入并直接输出
+	char ch;       //读取的字符，判断准则为ch不等于结束符EOF（end of file）
+	while (EOF != (ch = fgetc(fid)))
+		printf("%c", ch);
+}
+if (mode == 2)
+{
+	//注意变量初始化
+	char line[1024];
+	char *c=line;
+	//memset(line, 0, 1024);
+	while (!feof(fid))
+	{
+		fgets(c, 1024, fid);//改行文本，最大长度，指向文件的指针
+		printf("%s\n", c); //输出
+	}
+}
+/*if (mode == 3)
+{
+	//知道数据格式，按行读入并存储输出
+	int index_tmp;
+	double x_tmp, y_tmp;
+	while (!feof(fid))
+	{
+		fscanf(fid, "%d%lf%lf\n", &index_tmp, &x_tmp, &y_tmp);
+		index.push_back(index_tmp);
+		x_pos.push_back(x_tmp);
+		y_pos.push_back(y_tmp);
+	}
+	for (int i = 0; i < index.size(); i++)
+		printf("%04d\t%4.8lf\t%4.8lf\n", index[i], x_pos[i], y_pos[i]);
+
+}
+*/
+fclose(fid);
+
+}
