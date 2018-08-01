35728e65ebc85c9fa867bc9f486ff48e145d3c2e
forspy <qiubow@126.com>
2018-08-01 14:59:23 +0800

    测试_getche_getch_getchar的比较

---
+++ test_getchar_getch_getche.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+//#include<conio.h>
+//
+//int main()
+//{
+//	char ch;
+//	printf("输入一个字符：");
+//	ch = getch();
+//	printf("\n你输入的字符是：'%c'\n", ch);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<conio.h>
+//
+//int main()
+//{
+//	char ch;
+//	printf("输入一个字符：");
+//	ch = getche();
+//	printf("\n你输入的字符是：'%c'\n", ch);
+//	return 0;
+//}
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char ch;
+	printf("输入一个字符：");
+	ch = getchar();
+	printf("\n你输入的字符是：'%c'\n", ch);
+	return 0;
+}
