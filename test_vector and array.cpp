529e94ce893a447671966d5186b857a38959856f
forspy <qiubow@126.com>
2018-08-20 15:54:00 +0800

    测试_模板类vector和array+复习cin/cin.getline/getline
    vector储存在堆里，array储存在栈里，所以array的效率比较高
    vector <typename> vt(n_elem)  n_elem是一个变量也可以是常量
    array<typename,n_elem> arr ,array的n_elem不能是变量
    1.array的对象是可以赋值给另一个变量的2.变量的类型和大小必须一样

--- test_vector and array.cpp
+++ test_vector and array.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>//STL C++98 模板类
 #include<array>//C++11 模板类
+#include<string>
 
 int main(int agrc, char* argv[])
 {
@@ -26,6 +27,24 @@ int main(int agrc, char* argv[])
 	ai2 = ai;//1.array的对象是可以赋值给另一个变量的2.变量的类型和大小必须一样
 	cout << ai2[0] << endl;
 	ai2.at(0) = 9999;//也可以使用成员函数at()对数组赋值,可以防止数组下标越界
-	
+	//测试+复习
 	cout << ai2[0] << endl;
+	char* ptr = "hello";
+	cout << ptr << (int)ptr << (int*)"ss" << endl;
+	printf("%s\n", ptr);
+	string aa;
+	cin >>aa;//cin只能提取一个单词
+	cout << aa << endl;
+	//
+	cin.get();//1.为什么要加这一句，因为cin读取字符串的时候，将回车键生成的换行符留在了输入队列之中。
+	//		  //后面的cin.getline()看到换行符后，将认为这是一个空行，并将空字符赋值给下一个数组。
+	//		  //可以利用cin.get()来接住这个回车符。
+	//2.cin.get()只适用于接住一个空格或回车符，如果前面的输入是一行，则建议使用cin.getline() 或者getline（）
+	//
+	char bb[100];
+	cin.getline(bb,100);//cin.getline 是iosteam类的
+	cout << bb << endl;
+	string cc;
+	getline(cin, cc);//getline是string类的
+	cout << cc << endl;
 }
