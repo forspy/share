c73e148dd56700621c725991cf901478815c6c81
forspy <40792536+forspy@users.noreply.github.com>
2018-09-02 14:16:44 +0800

    测试_main 传入参数_argc_argv_atexit在main函数结束后再调用其它函数

--- test_c_main parameter_argc_argv[].c
+++ test_c_main parameter_argc_argv[].c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+void fun1(void);
+void fun2(void);
 int main(int argc, char* argv[])
 {
 	//agrc:传入参数的个数，默认1个开始
@@ -11,5 +13,15 @@ int main(int argc, char* argv[])
 	//方法一：cmd 打开.exe路径空格输入参数空格输入参数...
 	//方法二：shift选择多个文件，拖入.exe文件中
 	//方法三：项目属性-配置属性-调试-命令参数里输入参数，用空格隔开
+	atexit(fun1);
+	atexit(fun2);//atexit函数表示在main函数结束以后再调用，先调fun2，再调fun1，从下往上调用
 	system("pause");
+}
+void fun1(void)
+{
+	printf("1\n");
+}
+void fun2(void)
+{
+	printf("2\n");
 }
