317327bc5a2fb6e421eeb499621915bb7962f1b0
forspy <qiubow@126.com>
2018-09-04 15:16:44 +0800

    算法_c++_使用同一字符填充字符串

---
+++ algorithm_c++_string filled with same char.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+char* filled_with_same_char(char ch, int n);
+int main()
+{
+	int times=10;
+	char ch='a';
+	char* p;
+	p = filled_with_same_char(ch, times);
+	cout << p << endl;
+}
+char* filled_with_same_char(char ch, int n)
+{
+	char*ptr = new char[n + 1];//创建一个动态字符串数组,要创建多一位放'\0'
+	ptr[n] = '\0';
+	while (n-- >0)//如果是--n就是要>=0
+		ptr[n] = ch;//n:0-9
+	return ptr;
+}
