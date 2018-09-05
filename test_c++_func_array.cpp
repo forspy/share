15047c7fb615dbd0fa3191ce9ed6cd8ce989f432
forspy <qiubow@126.com>
2018-09-05 14:11:31 +0800

    测试_c++_array数组__指向对象的指针注意区别指向数组的指针_传递对象和传递对象地址_vector对象[迭代器]

--- test_c++_func_array.cpp
+++ test_c++_func_array.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<array>
 #include<string>
+#include<vector>
 using namespace std;
 const int Season = 4;
 const array < string, Season > sname= {"spring", "summer", "fall", "winter"};
@@ -10,9 +11,20 @@ void filled_expense(array<double, Season> *a, int n);
 void shou_expense(array<double, Season> ex, int n);
 int main(int argc, char** argv)
 {
-	array<double, Season>expense = {0,0,0,0};//expense是一个指针，也是一个对象，一个对象是一个个体（相当于是一个变量），一个数组是一个个体，数组里的元素只是里面的成员
+	int m[10] = {0};
+	array<double, Season>expense = {0,0,0,0};//expense不是一个指针，是一个对象，一个对象是一个个体（相当于是一个变量），一个数组是一个个体，数组里的元素只是里面的成员
 	filled_expense(&expense, Season);//传地址修改对象
 	shou_expense(expense, Season);//相当于拷贝副本输出
+	array<double, Season>* ptr = &expense;
+	//----------------------------附：数组与vector/array模板的比较
+	int* p = m;
+	vector<int> vi(100, 1);
+	vector<int>* pointer = &vi;
+	cout << p[1] << endl;//表示方式是解引用（地址）
+	cout << (*ptr)[1] << endl;//表示方式是对象[迭代器]
+	cout << (*pointer)[1]<< endl;//表示方式是对象[迭代器]
+	//cout << *(vi+1) << endl;vi是vector类的对象，对该对象不存在指针操作，同理array模板
+	
 
 }
 void filled_expense(array<double, Season>* a, int n)//其实a是一个指向指针的指针，因为array<double, Season>本身就是一个指针
