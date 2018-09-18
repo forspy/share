b4b1a115a297d97612f0c768f6f7629502f59dfc
forspy <40792536+forspy@users.noreply.github.com>
2018-09-18 08:14:39 +0800

    测试_c_树型结构_套用单链表模板

--- test_c_tree.c
+++ test_c_tree.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 //树是一种组织数据的方式
 //比如：文件夹->子文件夹->文件
 //比如：二叉树
@@ -38,16 +39,17 @@ struct item_node
 struct item_node* alloc_item_node(const char* name);
 void link_parent(struct tree_link* link, struct tree_link* parent);//在子节点的兄弟节点的下一个插入节点
 void trans_tree(struct tree_link* root);//先遍历根节点，后遍历字节点，先序递归
+void trans_tree_back(struct tree_link* root);
 int main()
 {
 	struct tree_link* tree_root = NULL;
 	struct item_node* root = alloc_item_node("A");//定义根树宿主A
 	//tree_root = &root->link;
-
+	//B C D都连A
 	struct item_node* node;
 	node = alloc_item_node("B");//定义子树宿主B
-	struct item_node* node_b = node;
-	link_parent(&node->link, &root->link);
+	struct item_node* node_b = node;//复制一下
+	link_parent(&node->link, &root->link);//传的是当前节点link的地址，以及A节点link的地址
 
 	node = alloc_item_node("C");
 	link_parent(&node->link, &root->link);
@@ -63,6 +65,8 @@ int main()
 	link_parent(&node->link, &node_b->link);
 
 	trans_tree(&root->link);
+	printf("-------\n");
+	trans_tree_back(&root->link);
 }
 
 struct item_node* alloc_item_node(const char* name)
@@ -75,18 +79,29 @@ struct item_node* alloc_item_node(const char* name)
 
 void link_parent(struct tree_link* link, struct tree_link* parent)//link表示要插入的节点，parent表示父节点
 {
-	link->parent = parent;//1式
+	link->parent = parent;//先将要插入的节点链上父节点，注意：parent链到的是堆上的内存
 	if (parent == NULL)
 		return;
-	struct tree_link** walk = &parent->children;//这里=link是否可以？不可以link是随机插入的节点，walk是要从子节点的头部开始遍历
+	struct tree_link** walk = &(parent->children);//walk是要从子节点的头部开始遍历,要修改一级指针的指向则需要用二级指针，
+	//为什么要用二级指针指向一级指针，因为：1.不能用一个一级指针结构体接住一个一级指针，相当于复制了一个庞大的链表
+	//2.创建的一级指针是在栈上的内存，在函数调用完毕后会被释放
 	while (*walk)
 	{
 		walk = &(*walk)->brother;//与1式的原理一样（*walk）是一个节点，与link一样
 	}
-	*walk = link;
+	*walk = link;//相当于给parent->children赋值,通过二级指针解引用给parent->children赋值
 }
+/*
+        A
+      / | \
+	 /  |  \
+    B - C - D
+   /|
+  / |
+ E -F
+*/    
 
-void trans_tree(struct tree_link* root)//先遍历根节点，后遍历字节点，先序递归
+void trans_tree(struct tree_link* root)//先遍历根节点，后遍历字节点，先序递归遍历
 {
 	struct item_node* node = LINK_TO_ELEM(root, struct item_node, link);
 	printf("%s\n", node->name);
@@ -97,4 +112,23 @@ void trans_tree(struct tree_link* root)//先遍历根节点，后遍历字节点，先序递归
 		trans_tree(walk);
 		walk = walk->brother;
 	}
+}
+
+//后序遍历，先遍历左子节点，兄弟节点，再父节点。
+void trans_tree_back(struct tree_link* root)
+{
+	
+	
+	struct tree_link* walk = root->children;
+	
+	while (walk)
+	{
+		trans_tree(walk);
+		
+		
+		walk = walk->brother;
+		
+	}
+	struct item_node* node = LINK_TO_ELEM(root, struct item_node, link);
+	printf("%s\n", node->name);
 }
