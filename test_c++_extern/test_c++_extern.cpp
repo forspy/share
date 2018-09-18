4a92f86f5cf666e38515753f68aacd28d293ede6
forspy <qiubow@126.com>
2018-09-18 16:28:50 +0800

    测试_c++_extern用法

--- test_c++_extern/test_c++_extern.cpp
+++ test_c++_extern/test_c++_extern.cpp
@@ -4,6 +4,13 @@ using namespace std;
 double warming = 0.3; // warming defined .
 void update(double dt);
 void local();
+//第一个const防止字符串被修改，第二个const确保数组中每个指针始终指向它最初指向的字符串。
+const char * const months[12] =
+{
+	"January","February","March","April","May",
+	"June","July","August","September","October",
+	"November","December"
+};
 int main(int argc, char** argv) {  //使用全局变量
 	cout << "Global warming is " << warming << "degress.\n";
 	update(0.1);                 //call function to change warming
