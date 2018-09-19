746bc8e02ee297c4a66803f4634aa6fa9039a5bf
forspy <qiubow@126.com>
2018-09-19 09:50:50 +0800

    测试_c++_new_定位new_(void*)强制类型转换

--- test_c++_new.cpp
+++ test_c++_new.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
-
+#include<new>//定位new运算符头文件，让new指定要使用的位置
+struct test
+{
+	char name[20];
+	int num;
+};
+char buffer1[50];
+char buffer2[100];
 int main()
 {
 	//new初始化
@@ -14,4 +21,10 @@ int main()
 	where* one = new where{1.0, 2.0};
 	int* ar = new int[4]{ 1,2,3,4 };
 	delete[] ar;
+	//--------定位运算符
+	test* p1 = new (buffer1) test;//在全局数组中buffer1中分配内存给结构test
+	int* p2 = new(buffer2) int[20];//在全局数组中buffer2中分配内存给20个int类型
+	std::cout << (void*)buffer2 << std::endl;//因为p2的类型与buffer2的类型不同，如果不转换将输出字符串，所以需要对buffer2做(void*)类型的转换
+	//这样得到buffer2的地址，而buffer2里面存的是int类型，所以cout会输出int类型的值
+	//不能用delete或delete[]释放，因为定位new开辟在静态区
 }
