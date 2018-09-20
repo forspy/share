96b57582938cc6b3fd352a0564f321e39c55294e
forspy <qiubow@126.com>
2018-09-20 17:01:53 +0800

    测试_类_类对象指针_this指针指向显式对象_返回引用对象_const成员函数用法

---
+++ test_c++_Class 3/stock03.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include"stock03.h"
+
+//构造函数，实现部分（方式：重载）
+Stock::Stock()//构造函数没有返回值
+{
+	std::cout << "调用默认构造函数\n";
+	company = "No company";
+	shares = 0;
+	share_val = 0.0;
+	set_tot();
+}
+Stock::Stock(const std::string& co, long n, double pr)//1.Stock::(类+作用域解析运算符)表示该成员函数能够使用这个类的所有成员2.使用引用接住string对象
+{
+	//std::cout << "使用公司名（第一个参数），采用默认参数的方式调用构造函数\n";
+	company = co;
+	if (n < 0)
+	{
+		std::cout << "持股数目不能为负数，设为0\n";
+		shares = 0;
+	}
+	else
+		shares = n;
+	share_val = pr;//接住每股价格
+	set_tot();//计算私有成员中total_val的总价格
+}
+//析构函数的实现
+Stock::~Stock()
+{
+	std::cout << "Done!\n";//这里虽然没有使用new开辟内存，所以析构函数不会发挥很大作用，但至少可以看到Stock的对象的生命周期
+}
+//Stock::(类+作用域解析运算符)表示该成员函数能够使用这个类的所有成员2.使用引用接住string对象
+void Stock::buy(long num, double price)//买入几股
+{
+	if (num < 0)
+		std::cout << "买入不能为负数\n";
+	else
+	{
+		shares += num;
+		share_val = price;//股票现价
+		set_tot();//重新计算
+	}
+}
+
+void Stock::sell(long num, double price)//卖出几股
+{
+	using std::cout;//使用名称空间方便一点
+	if (num < 0)
+		cout << "卖出不能为负数\n";
+	else if (num > shares)
+		cout << "卖出数目不能大于现持数目\n";
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)//股价变动
+{
+	share_val = price;
+	set_tot();
+}
+
+void Stock::show() const
+{
+	using std::endl;
+	std::ios_base::fmtflags orig = std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);//保存默认输出格式
+																								   //std是名称空间，ios_base是名称空间下的类，ftmflags是该类下的一种类型 orig是变量储存了输出格式  fixed是保持小数输出，floatfield包含定点表示和科学表示
+	std::streamsize prec = std::cout.precision(3);//记录默认输出小数点后三位
+	std::cout << "公司：" << company << endl
+		<< "持股数：" << shares << endl
+		<< "股价：" << share_val << endl;
+	std::cout.precision(2);//以小数点后两位输出总价
+	std::cout << "该股总价值" << total_val << endl;
+	//还原默认设置
+	std::cout.setf(orig);
+	std::cout.precision(prec);
+}
+
+//利用this指针，比较两个对象中的较大值，返回一个该对象的引用（就为了省一个参数...niub）
+//1.this指针用来指向调用成员函数的对象
+//2.通过指针来访问结构成员同样适用于类成员(重要)
+const Stock& Stock::topval(const Stock& s) const
+{
+	if (s.total_val > total_val)//其实total_val为this->total_val
+		return s;
+	else
+		return *this;//返回调用成员函数的对象
+}
---
+++ test_c++_Class 3/test_c++_Class 3.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include"stock03.h"
+
+int main()
+{
+	{
+		//获得股票
+		//Stock business("qiushi", 50, 2.5);//隐式的创建一个名为business的类对象，并赋给这个对象初值，使用默认参数型的构造函数   
+										  //business.show();
+		Stock business = Stock("qiushi", 50, 2.5);//显式的创建一个名为business的类对象，并赋给这个对象初值，需要赋值,推荐	
+		business.show();
+		//增持
+		business.buy(10, 3.5);
+		business.show();
+		//减持
+		business.sell(20, 3.3);
+		business.show();
+		//不增持也不减持
+		business.update(3.6);
+		business.show();
+		Stock copy;//将使用重载型的默认构造函数
+		copy.show();
+		copy = business;//对象可以拷贝，拷贝的原理是将该类的所有成员都拷贝到目标对象中，所以目标对象的成员都被替换了一遍，其中也包含析构函数，因此总体调用了析构函数两次
+		copy.show();
+		Stock* p = new Stock{ "test",10,10 };
+		(*p).show();
+		delete p;//不能用delete[] p
+				 //1.析构函数只会自动释放释放类里面定义的指向堆上的指针，类对象的指针需要自己使用delete释放完以后才会调用析构函数
+				 //2.析构函数的调用时间点时在类对象作用域完结时析构函数上场，因为指向堆的指针对象在堆上开辟内存，所以只有在该内存释放以后才会调用这个类指针的析构函数
+	}//加这个大括号的目的时在这个代码段调用析构函数，使内存释放
+	Stock my_company[4] = {
+		Stock("疼讯",50,2.5),
+		Stock("阿里爸爸",70,2.4),
+		Stock("白度",30,2.2),
+		Stock("邱氏",10,2),
+	};
+	for (int i = 0; i < 4; i++)
+		my_company[i].show();
+	//利用this指针比较市值最大的公司
+	std::cout << "-----市值最大的是-------\n";
+	//1.先定义一个指针指向类数组的第一个元素
+	const Stock* ptr = &my_company[0];
+	for (int i = 0; i < 4; i++)
+	{
+		ptr = &ptr->topval(my_company[i]);//2.比较ptr指向的对象与i计数中的对象的大小，使指针指向较大市值的对象（注意对象指针对成员函数的使用->）
+	}
+	//3.显示最大的对象
+	ptr->show();//注意要对show（）后面加const
+}
