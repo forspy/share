e96d91307fca78a85babcc41cc9deccc7755c7be
forspy <40792536+forspy@users.noreply.github.com>
2018-09-21 16:49:41 +0800

    测试_c++_类_抽象数据类型_ADT_栈_push_pop_bool_cctype_清除多余缓存字母_封装_隐藏_抽象

--- test_c++_abstract data type_ADT_stack/stack.h
+++ test_c++_abstract data type_ADT_stack/stack.h
@@ -1,16 +1,20 @@
 #pragma once
 /*
-������������ADT_abstract data type
-��ջִ�еز�����
-������ջ
-ѹ��ջ�����������ӵ�ջ��
-����ջ����ջ��ɾ��������
-�鿴ջ�Ƿ�����
-�鿴ջ�Ƿ�Ϊ��
+关于类的抽象、隐藏和封装
+抽象：表示用户可以用类方法地公有接口对类对象执行操作
+隐藏：类的数据成员可以是私有地，这意味着只能通过成员函数来访问这些数据
+封装：成员函数的实现细节，如数据表示和方法地代码都是隐藏地
+抽象数据类型ADT_abstract data type
+对栈执行地操作：
+创建空栈
+压入栈：将数据添加到栈顶
+弹出栈：从栈顶删除数据项
+查看栈是否填满
+查看栈是否为空
 
-ͨ��������ʵ��ADT��
-���г�Ա��ʾջ�����ؽӿ�
-˽�г�Ա����ջ���ݣ����淽ʽ����ʹ�ó������顢��̬���顢����߼����ݽṹ������������
+通过类声明实现ADT：
+公有成员表示栈操作地接口
+私有成员储存栈数据（储存方式可以使用常规数组、动态数组、或更高级数据结构（如链表））
 */
 #ifndef STACK_H_
 #define STACK_H_
@@ -23,11 +27,11 @@ private:
 public:
 	Stack();
 	~Stack();
-	bool isempty() const;//bool���ͣ�����true/false
+	bool isempty() const;//bool类型，返回true/false
 	bool isfull() const;
-	bool push(const long& item);//���ջ���˷���false�����򷵻�true
-	bool pop(long& item);//���ջ���˷���false�����򷵻�true
-	void show();//����˽�в��ֵ�����
+	bool push(const long& item);//如果栈满了返回false，否则返回true
+	bool pop(long& item);//如果栈空了返回false，否则返回true
+	void show();//访问私有部分地数据
 };
 
 #endif
