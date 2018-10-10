5a32efb5f62eade3c3a31d9b20323a927cf12c22
forspy <40792536+forspy@users.noreply.github.com>
2018-10-10 21:28:27 +0800

    linux虚拟机安装

--- linux虚拟机安装.cpp
+++ linux虚拟机安装.cpp
@@ -15,18 +15,24 @@ sudo apt-get install gcc
 sudo apt-get install g++
 sudo apt-get install vim-gtk
 
-vi main.c 新建一个vim文档
-按i输入 按esc键跳出 ：wq 保存退出  ：q！不保存退出  ：set nu 显示行号
-使用gcc main.c 可以看到多了一个a.out  
-运行a.out  ./a.out  （./表示当前文件夹  ../表示上一层文件夹）
+虚拟机 sata0:1找不到 是因为主机没有光驱  需要把光驱中的启动时连接勾掉
 如果ubuntu不能自适应窗口需要安装VMware tools 
 注意需要把VMware tools 光盘拷出只读文件夹tar （或者）解压 运行 ./vmware-install.pl  如果需要以管理员身份运行可以输入 sudo ./vmware-install.pl
-ubuntu 复制黏贴 ctrl+shift+c ctrl+shift+v
+
 */
 /*
 linux常用命令：
+在Ubuntu中打开终端的快捷键是Ctrl+Alt+T
+ubuntu 复制黏贴 ctrl+shift+c ctrl+shift+v
 rm -rf 文件夹名称    删除所有文件和子文件夹
 mkdir 文件名  如果想建立有空格的文件名需要'a b'
+
+vi main.c 新建一个vim文档
+按i输入 按esc键跳出 ：wq 保存退出  ：q！不保存退出  ：set nu 显示行号
+使用gcc main.c 可以看到多了一个a.out
+运行a.out  ./a.out  （./表示当前文件夹  ../表示上一层文件夹）
+
+
 解压：
 tar -xvf file.tar //解压 tar包
 
