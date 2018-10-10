332a721783381953f91a10f525e2a575daf9a41e
forspy <qiubow@126.com>
2018-10-10 15:23:22 +0800

    linux虚拟机安装

--- linux虚拟机安装.cpp
+++ linux虚拟机安装.cpp
@@ -11,12 +11,32 @@ sudu su  然后输入密码  #表示root用户
 
 通过管理员权限安装gcc（c）、g++（c++）编译器和vim编辑器
 指令为：
-sudu apt-get install gcc
+sudo apt-get install gcc
 sudo apt-get install g++
-sudu apt-get install vim-gtk
+sudo apt-get install vim-gtk
 
 vi main.c 新建一个vim文档
 按i输入 按esc键跳出 ：wq 保存退出  ：q！不保存退出  ：set nu 显示行号
 使用gcc main.c 可以看到多了一个a.out  
 运行a.out  ./a.out  （./表示当前文件夹  ../表示上一层文件夹）
+如果ubuntu不能自适应窗口需要安装VMware tools 
+注意需要把VMware tools 光盘拷出只读文件夹tar （或者）解压 运行 ./vmware-install.pl  如果需要以管理员身份运行可以输入 sudo ./vmware-install.pl
+ubuntu 复制黏贴 ctrl+shift+c ctrl+shift+v
+*/
+/*
+linux常用命令：
+rm -rf 文件夹名称    删除所有文件和子文件夹
+mkdir 文件名  如果想建立有空格的文件名需要'a b'
+解压：
+tar -xvf file.tar //解压 tar包
+
+tar -xzvf file.tar.gz //解压tar.gz
+
+tar -xjvf file.tar.bz2   //解压 tar.bz2
+
+tar -xZvf file.tar.Z   //解压tar.Z
+
+unrar e file.rar //解压rar
+
+unzip file.zip //解压zip
 */
