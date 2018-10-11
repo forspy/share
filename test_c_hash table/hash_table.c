9befcd5110334f65a667b3efed815e2abd1f07cf
forspy <40792536+forspy@users.noreply.github.com>
2018-10-11 15:41:17 +0800

    项目_c_hash表

--- test_c_hash table/hash_table.c
+++ test_c_hash table/hash_table.c
@@ -3,31 +3,31 @@
 #include<string.h>
 
 #include"hash_table.h"
-//����hash����
+//创建hash集合
 struct hash_table* create_hash_table(int n)
 {
-	struct hash_table* t = (struct hash_table*)malloc(sizeof(struct hash_table));//��������ѽ��ʵ����ֻ������4�ֽڣ�����ָ��Ҳ��ָ�룩+4�ֽ�(int)�����Զ���ָ��������Ҫ���������ڴ�
+	struct hash_table* t = (struct hash_table*)malloc(sizeof(struct hash_table));//待解决？已解决实际上只开辟了4字节（二级指针也是指针）+4字节(int)，所以对于指针数组需要继续开辟内存
 	memset(t, 0, sizeof(struct hash_table));
-	t->hash_set = malloc(n * sizeof(struct hash_node*));//��������һ��n�����ϵ�����ͷָ��Ŀռ䣬ΪʲôҪ�������ٿռ䣬��������ѽ�� sizeof(struct hash_node*)ָ����һ��ָ�룬һ��n��ָ��
+	t->hash_set = malloc(n * sizeof(struct hash_node*));//用来开辟一共n个集合的链表头指针的空间，为什么要继续开辟空间，待解决？已解决 sizeof(struct hash_node*)指的是一个指针，一共n个指针
 	memset(t->hash_set, 0, n * sizeof(struct hash_node*));
 	t->n = n;
 	return t;
 }
-//�ͷ�hash����,ɾ�����е�Ԫ��
+//释放hash集合,删掉所有的元素
 void destroy_hash_table(struct hash_table* t)
 {
-	//1.��ɾ��������ʹ��malloc�����Ľڵ��key
+	//1.先删除集合内使用malloc创建的节点和key
 	hash_clear(t);
-	//2.��ɾ��hash_set���ٵ�n������
+	//2.再删除hash_set开辟的n个集合
 	if (t->hash_set)
 	{
 		free(t->hash_set);
 		t->hash_set = NULL;
 	}
-	//3.���ɾ��t���ٵ�8���ֽ� 
+	//3.最后删除t开辟的8个字节 
 	free(t);
 }
-//hash�㷨
+//hash算法
 unsigned int hash_index(char *str)
 {
 	register unsigned int h;
@@ -40,34 +40,38 @@ unsigned int hash_index(char *str)
 }
 
 
-//hash���룬����һ��key��value�����ж����ظ���������ͷ������
+//hash插入，插入一个key和value，不判断有重复，反复再头部插入
 void hash_insert(struct hash_table* t, char* key, void* value)
 {
-	//������һ��hash�ڵ�
+	//先申请一个hash节点
 	struct hash_node* node = (struct hash_node*)malloc(sizeof(struct hash_node));
 	memset(node, 0, sizeof(struct hash_node));
-	//�ַ������ƣ�ע������key��һ��ָ�룬����strup�ڶ��Ͽ��ٿռ䣬��Ϊ���ٿռ��ʱ�������char������п���
-	//ֻ����������ָ��Ŀռ�һ��12�ֽ�ɾ��ʱע����Ҫ������node->key�����ͷ�
+	//字符串复制，注意这里key是一个指针，是用strup在堆上开辟空间，因为开辟空间的时候不是针对char数组进行开辟
+	//只开辟了三个指针的空间一共12字节删除时注意需要单独对node->key进行释放
 	node->key = strdup(key);
-	node->value = value;//������Ϣ����
+	node->value = value;//具体信息复制
 
-	unsigned int index = hash_index(key)%t->n;//ʹ��hash�㷨������key�����ĸ�����
+	unsigned int index = hash_index(key)%t->n;//使用hash算法来返回key属于哪个集合
 
-	struct hash_node* header = t->hash_set[index];//1.t->hash_set[index]��ʾһ��ָ�����������һ��ָ�룬walk��ʾͷָ��ָ�����λ��
-	//2.ͨ��t���ʵ���main �е�hash_set���ı���main�е�hash_set��ָ��
-	//����ͷ�巨
-	node->next = header;//����t->hash_set[index]��һ����ַ
-	t->hash_set[index] = node;//����t->hash_set[index]��һ��������ָ�룩
+	struct hash_node* header = t->hash_set[index];//1.t->hash_set[index]表示一个指针数组的其中一个指针，walk表示头指针指向这个位置
+	//2.通过t访问到了main 中的hash_set，改变了main中的hash_set的指向
+	//链表头插法
+	
+	//node->next = header;//这里t->hash_set[index]是一个地址(辅助代码，表示这样也行，更容易理解)
+	//指针在右值和左值表示的意义不同，在右值表示地址，在左值表示变量
+	//普通变量在左值和在右值时的意义也不一样，在右值表示值，在左值表示变量
+	node->next = t->hash_set[index];//这里t->hash_set[index]是一个地址
+	t->hash_set[index] = node;//这里t->hash_set[index]是一个变量（指针）
 }
-//hash���룬������һ����ͬ��key��ʱ�򣬲���ͷ�����룬���Ǹ������key��Ӧ������
+//hash插入，当输入一个相同的key的时候，不在头部插入，而是覆盖这个key对应的内容
 void hash_set(struct hash_table* t, char* key, void* value)
 {
-	//1.���key���index�����ĸ�����
-	unsigned int index = hash_index(key) % t->n;//ʹ��hash�㷨������key�����ĸ�����
+	//1.针对key求得index属于哪个集合
+	unsigned int index = hash_index(key) % t->n;//使用hash算法来返回key属于哪个集合
 
-	//2.��Ϊ��Ҫ�ж�keyֵ�Ƿ���ͬ��������Ҫʹ�ö���ָ�����һ�鵥�������ı������е�ֵ
+	//2.因为需要判断key值是否相同，所以需要使用二阶指针遍历一遍单链表，改变链表中的值
 	struct hash_node** walk = &(t->hash_set[index]);
-	//����
+	//遍历
 	while (*walk)
 	{
 		if (strcmp((*walk)->key, key) == 0)
@@ -77,95 +81,95 @@ void hash_set(struct hash_table* t, char* key, void* value)
 		}
 		walk = &((*walk)->next);
 	}
-	//3.������ɺ���û�У�����Ҫ��������ڵ㣬����ʹ��ͷ�巨
-	//������һ��hash�ڵ�
+	//3.遍历完成后发现没有，则需要重新申请节点，这里使用头插法
+	//先申请一个hash节点
 	struct hash_node* node = (struct hash_node*)malloc(sizeof(struct hash_node));
 	memset(node, 0, sizeof(struct hash_node));
-	//ע������key��һ��ָ�룬����strup�ڶ��Ͽ��ٿռ䣬��Ϊ���ٿռ��ʱ�������char������п���
-	//ֻ����������ָ��Ŀռ�һ��12�ֽ�ɾ��ʱע����Ҫ������node->key�����ͷ�
-	node->key = strdup(key);//�ַ������ƣ�strdup�൱����malloc,��strcpy
-	node->value = value;//������Ϣ����
+	//注意这里key是一个指针，是用strup在堆上开辟空间，因为开辟空间的时候不是针对char数组进行开辟
+	//只开辟了三个指针的空间一共12字节删除时注意需要单独对node->key进行释放
+	node->key = strdup(key);//字符串复制，strdup相当于先malloc,再strcpy
+	node->value = value;//具体信息复制
 
-	struct hash_node* header = t->hash_set[index];//1.t->hash_set[index]��ʾһ��ָ�����������һ��ָ�룬walk��ʾͷָ��ָ�����λ��
-												  //2.ͨ��t���ʵ���main �е�hash_set���ı���main�е�hash_set��ָ��
-												  //����ͷ�巨
-	node->next = header;//����t->hash_set[index]��һ����ַ
-	t->hash_set[index] = node;//����t->hash_set[index]��һ��������ָ�룩
+	struct hash_node* header = t->hash_set[index];//1.t->hash_set[index]表示一个指针数组的其中一个指针，walk表示头指针指向这个位置
+												  //2.通过t访问到了main 中的hash_set，改变了main中的hash_set的指向
+												  //链表头插法
+	node->next = header;//这里t->hash_set[index]是一个地址
+	t->hash_set[index] = node;//这里t->hash_set[index]是一个变量（指针）
 
-	//�����ʹ��ͷ���ʹ��β��Ļ�����: *walk=node; ��Ϊ�ղű�����ʱ��*walk�Ѿ��ߵ���NULL
+	//如果不使用头插而使用尾插的话就是: *walk=node; 因为刚才遍历的时候*walk已经走到了NULL
 }
-//hash����
+//hash查找
 void* hash_find(struct hash_table* t, char* key)
 {
-	//1.���key���index�����ĸ�����
-	unsigned int index = hash_index(key) % t->n;//ʹ��hash�㷨������key�����ĸ�����
-	//2.��������
-	struct hash_node* walk = t->hash_set[index];//��Ϊ����ֻ��Ҫ���Ҷ�����Ҫ�ı�����������ֻʹ��һ��ָ���������
-	//����
+	//1.针对key求得index属于哪个集合
+	unsigned int index = hash_index(key) % t->n;//使用hash算法来返回key属于哪个集合
+	//2.遍历查找
+	struct hash_node* walk = t->hash_set[index];//因为这里只需要查找而不需要改变链表，所以只使用一阶指针遍历就行
+	//遍历
 	while (walk)
 	{
 		if (strcmp(walk->key, key) == 0)
 			return walk->value;
 		walk = walk->next;
 	}
-	//û���ҵ��򷵻�NULL
+	//没有找到则返回NULL
 	return NULL;
 }
 
-//ɾ��hash���е���
+//删除hash表中的项
 void hash_delete(struct hash_table* t, char* key)
 {
-	//1.���key���index�����ĸ�����
-	unsigned int index = hash_index(key) % t->n;//ʹ��hash�㷨������key�����ĸ�����
+	//1.针对key求得index属于哪个集合
+	unsigned int index = hash_index(key) % t->n;//使用hash算法来返回key属于哪个集合
 
-	//2.��Ϊ��Ҫ�ı�������������Ҫʹ�ö���ָ�����һ�鵥�������ж�keyֵ�Ƿ���ͬ��ɾ�������еĽڵ�
+	//2.因为需要改变链表，所以需要使用二阶指针遍历一遍单链表，判断key值是否相同，删除链表中的节点
 	struct hash_node** walk = &(t->hash_set[index]);
 
-	//3.����ɾ���ڵ㲢�ͷ�
+	//3.遍历删除节点并释放
 	while (*walk)
 	{
-		if (strcmp((*walk)->key, key) == 0)//���ʹ��hash_insert�п���һ��key��Ӧ���value
+		if (strcmp((*walk)->key, key) == 0)//如果使用hash_insert有可能一个key对应多个value
 		{
 			struct hash_node* temp = *walk;
 			*walk = (*walk)->next;
 			temp->next = NULL;
-			free(temp->key);//��free strdup���ٳ�����key
-			free(temp);//��free�����Ľڵ�
+			free(temp->key);//先free strdup开辟出来的key
+			free(temp);//再free孤立的节点
 		}
 		else
-			walk = &((*walk)->next);//if�е�*walk = (*walk)->next;�б����ĸ���Ч�����������������Ҫelse
+			walk = &((*walk)->next);//if中的*walk = (*walk)->next;有遍历的附加效果，所以这里遍历需要else
 	}
 }
 
-//ɾ��hash��׼��������ɾ�������е�ÿ���ڵ�Ͷ�Ӧ��key
-void hash_clear(struct hash_table* t)//ע�⣬����������Ҫÿ���ڵ�һ��һ��ɾ����˭�����˿ռ��ɾ����Ӧ��ָ��
+//删除hash表准备工作：删除集合中的每个节点和对应的key
+void hash_clear(struct hash_table* t)//注意，对于链表需要每个节点一个一个删除，谁开辟了空间就删除对应的指针
 {
-	//1.������ͬ�ļ���
+	//1.遍历不同的集合
 	for (int i = 0; i < t->n; i++)
 	{
-		//2.����ĳ�������е�����,��ɾ��key�ͽڵ�
+		//2.遍历某个集合中的链表,并删除key和节点
 		struct hash_node* walk = t->hash_set[i];
 		while (walk)
 		{
 			struct hash_node* temp = walk;
 			walk = walk->next;
 			temp->next = NULL;
-			//ɾ��key�ͽڵ�
+			//删除key和节点
 			free(temp->key);
 			free(temp);
 		}
 	}
 }
 
-//��ʾhash��������Ԫ��
-//����һ��key���϶�Ӧ������֣����Ը���key���Ƴ����Ʋ�����
+//显示hash表的所有元素
+//但是一个key集合对应多个名字，所以根据key逆推出名称不可行
 void show_all(struct hash_table* t)
 {
-	//1.������ͬ�ļ���
+	//1.遍历不同的集合
 	for (int i = 0; i < t->n; i++)
 	{
-		printf("��%d�����ϣ�", i);
-		//2.����һ�����ϵ�����
+		printf("第%d个集合：", i);
+		//2.遍历一个集合的链表
 		struct hash_node* walk = t->hash_set[i];
 		while (walk)
 		{
