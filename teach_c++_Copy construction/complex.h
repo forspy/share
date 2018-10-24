43405d8fad9a84cd1fe908b388956095adf0c681
forspy <40792536+forspy@users.noreply.github.com>
2018-10-24 23:21:21 +0800

    教学_c++_拷贝构造

---
+++ teach_c++_Copy construction/complex.h
@@ -0,0 +1,54 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class Complex
+{
+private:
+	double m_real;
+	double m_vir;
+public:
+	Complex();
+	Complex(double real);//单参类型转换构造函数 用explicit可以防止隐式使用 比如complex a=1.2;不行 
+	Complex(const Complex& that);//单参同类型构造
+	~Complex();
+	void print()const;
+};
+
+class ninjia
+{
+private:
+	int* m_age;
+public:
+	ninjia()
+	{
+		cout << "默认构造" << endl;
+	}
+	explicit ninjia(int age) :m_age(NULL)
+	{
+		if (age >= 0)
+		{
+			m_age = new int(age);
+		}
+		cout << "单参构造" << endl;
+	}
+
+	ninjia(const ninjia& that)
+	{
+		this->m_age = new int;
+		*m_age = *(that.m_age);
+		//m_age = that.m_age;
+	}
+	~ninjia()
+	{
+		if (m_age)
+		{
+			delete m_age;
+			//m_age = NULL;
+		}
+		cout << "析构" << endl;
+	}
+	void show()
+	{
+		cout << *m_age << "岁" << endl;
+	}
+};
