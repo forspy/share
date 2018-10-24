fba844221372cd35a92557b59364164fca5e7867
forspy <40792536+forspy@users.noreply.github.com>
2018-10-24 15:04:41 +0800

    测试_c++_类自动类型转换_构造函数_explicit_类转换成其他类型

--- test_c++_auto type conversion.cpp
+++ test_c++_auto type conversion.cpp
@@ -1,17 +1,18 @@
 /*
-C++��������ͻ�������֮�����ת��
-���磺
-������һ�����캯��Stack(double a)
-������Զ����Stack money=1.6��ת������ʽת����
-������һ�����캯��Stack(char* a)
-������Զ����Stack name="hello world"��ת������ʽת����
-����ڹ��캯��ǰ��������explicit(����Ϊ��ȷ��),��ù��캯��ֻ��������ʽ��ת�� Stack name=Stack("hello world");
-�������ת�����������ͣ����붨��ת������operator typename()
-1.ת�����������ǳ�Ա����
-2.ת������û�з������͡�û�в����������뷵��ת�����ֵ
+C++允许在类和基本类型之间进行转换
+比如：
+类中有一个构造函数Stack(double a)
+则可以自动完成Stack money=1.6的转换（隐式转换）
+类中有一个构造函数Stack(char* a)
+则可以自动完成Stack name="hello world"的转换（隐式转换）
+如果在构造函数前面声明了explicit(翻译为明确的),则该构造函数只能用于显式地转换 Stack name=Stack("hello world");
+将类对象转换成其他类型，必须定义转换函数operator typename()
+1.转换函数必须是成员函数
+2.转换函数没有返回类型、没有参数，但必须返回转换后地值
 Vector::operator double()
 {
 ...
 	return a_double_value;
 }
+使用转换函数要小心，可以在声明构造函数时使用关键字explicit，以防止它被用于隐式转换
 */
