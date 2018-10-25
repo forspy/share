8915dbf01d8e8aa9240cdffc187e458f81f03e7d
forspy <40792536+forspy@users.noreply.github.com>
2018-10-25 20:29:21 +0800

    教学_c++_拷贝构造

--- teach_c++_Copy construction/teach_c++_Copy construction.cpp
+++ teach_c++_Copy construction/teach_c++_Copy construction.cpp
@@ -2,18 +2,19 @@
 
 int main()
 {
-	Complex cp1(1.2);//��ʾ����
+	Complex cp1(1.2);//显示调用
 	cp1.print();
 	Complex cp2 = 1.2;
-	//Complex cp2 = Complex(1.2);//��ʽ����  complex cp2=complex(1.2)  ������̲���������
+	//Complex cp2 = Complex(1.2);//隐式调用  complex cp2=complex(1.2)  这个过程不存在析构
 	cp2.print();
 
-	Complex cp3 = cp1;//��ʽ����
+	Complex cp3 = cp1;//隐式拷贝
+	//cp3=cp1这样不是拷贝构造，而是=重载，因为cp3已经构造出来了
 	cp3.print();
-	Complex cp4(cp1);//��ʽ����
+	Complex cp4(cp1);//显式拷贝
 	cp4.print();
 
-	//new��������
+	//new拷贝构造
 	Complex* cp5 = new Complex(cp1);
 
 	ninjia* kakaxi = new ninjia(20);
@@ -22,6 +23,6 @@ int main()
 	n1->show();
 	
 	delete kakaxi;
-	delete n1;//����Ǳ������ṩ��Ĭ�Ͽ������캯���򲻻������ȿ�����������Ҫ���
-	//Ϊ��Ա�����������¿����ڴ棬��̬��ԱҲ������Ҫʹ�ÿ���������=��ʵ�θ����βΣ�returnʱ������ÿ�������
+	delete n1;//如果是编译器提供的默认拷贝构造函数则不会进行深度拷贝，所以需要深拷贝
+	//为成员函数函数回新开辟内存，静态成员也可能需要使用拷贝构造在=，实参赋给形参，return时都会调用拷贝构造
 }
