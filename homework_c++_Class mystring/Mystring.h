d05bb382847afedaa24059d6ca5c48713be2b5a6
forspy <40792536+forspy@users.noreply.github.com>
2018-11-02 19:05:31 +0800

    作业_c++_Mystring类实现_重载_代码复用_=值是调用带参构造函数

--- homework_c++_Class mystring/Mystring.h
+++ homework_c++_Class mystring/Mystring.h
@@ -10,11 +10,16 @@ public:
 	Mystring();
 	Mystring(char* str );
 	Mystring(const Mystring& mystr);
+	//= []必须为成员函数
+	Mystring& operator=(const Mystring& mystr);
+	char operator[](int n);
+	int length();
 	//双目运算符(建议友元)
 	friend Mystring operator+(const Mystring& mystr1, const Mystring mystr2);
 	friend Mystring& operator+=(Mystring& mystr1,const Mystring& mystr2);
 	//<< >>必须为友元
 	friend ostream& operator<<(ostream& os, const Mystring& mystr);//注意重载<<要使用using namespace std;
+	friend istream& operator >> (istream& is, Mystring& mystr);//要修改的对象不能const
 	~Mystring();
 };
 
--- homework_c++_Class mystring/homework_c++_Class mystring.cpp
+++ homework_c++_Class mystring/homework_c++_Class mystring.cpp
@@ -2,11 +2,22 @@
 
 int main()
 {
+	char* p = "test";
 	Mystring A;
+	A = p;//这一步调用带参构造函数
+	cout << A << endl;
 	Mystring B("hello");
 	Mystring C(" world!!!");
 	Mystring D = B + C;
-	cout << D << endl;
+	cout << B<<"+"<<C<<"="<<D << endl;
+
+	cout << B << "+=" << C << "为";
 	B += C;
 	cout << B << endl;
+	Mystring E;
+	cin >> E;
+	cout <<"输入结果为："<< E << endl;
+	int i = 1;
+	cout << "第"<<i<<"个字符是"<<E[i] << endl;
+	cout << E << "的长度为" << E.length() << endl;
 }
