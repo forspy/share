169507e85b1aafba59d77fa3e892e4aa2a10f260
forspy <40792536+forspy@users.noreply.github.com>
2018-11-16 15:33:37 +0800

    教学_c++_类继承虚函数_函数指针_虚函数表_二级指针

--- teach_c++_Polymorphism/teach_c++_Polymorphism.cpp
+++ teach_c++_Polymorphism/teach_c++_Polymorphism.cpp
@@ -1,6 +1,6 @@
-//��̳���ר������������μ̳е�����ģ���Ϊ���ʵ�̳еĻ��������������涼���л��ಿ��
-//���ò����޸Ķ���ָ������޸�ָ�����������ڶ�̬����ı���������
-//�Ժ���ָ��Ķ�̬��Ϊ��
+//虚继承是专门用来解决菱形继承的问题的，因为如果实继承的话两个派生类里面都会有基类部分
+//引用不能修改对象，指针可以修改指向，所以引用在多态方面的表现力较弱
+//以函数指针的多态性为例
 #include<iostream>
 #include"test.h"
 using namespace std;
@@ -33,8 +33,8 @@ int call_math(int x, int y, func funcName)
 class A
 {
 public:
-	A(){ cout << "A����" << endl; }
-	virtual ~A() { cout << "A����" << endl; }
+	A(){ cout << "A构造" << endl; }
+	virtual ~A() { cout << "A析构" << endl; }
 	virtual void test() { cout << "A:test" << endl; }
 };
 class B :public A
@@ -42,26 +42,26 @@ class B :public A
 private:
 	int* p;
 public:
-	B() { cout << "B����" << endl; p = new int[10]; }
-	virtual ~B() { cout << "B����" << endl; delete[] p; }//����д��virtual��������Ҳ��Ĭ��virtual
+	B() { cout << "B构造" << endl; p = new int[10]; }
+	virtual ~B() { cout << "B析构" << endl; delete[] p; }//基类写了virtual，派生类也会默认virtual
 	virtual void test() { cout << "B:test" << endl; }
 };
-//������� virtual int func����=0�����麯�������󷽷���ֻ�д��麯��������������ࣨ�ӿ��ࣩ��������಻��ʵ��������
-//2.�̳д���������Ҫ�����еĳ��󷽷�ȫ��ʵ�֣������һ��ûʵ�֣���ô���������Ϊ�������
+//抽象基类 virtual int func（）=0（纯虚函数）抽象方法，只有纯虚函数的类叫做纯虚类（接口类），抽象基类不能实例化对象
+//2.继承纯抽象类需要把所有的抽象方法全部实现，如果有一个没实现，那么该派生类成为抽象基类
 
-//��һ�ַ���ʵ���麯��������ָ�����
+//另一种方法实现虚函数表函数指针调用
 void CallVirtualFunc(void *pthis, int index = 0)
 {
-	void(*funptr)(void*);//����һ������ָ��
-	long vptrAddr;//�����麯������ַ
-	memcpy(&vptrAddr, pthis, 4);//�Ѷ����ǰ4���ֽ�Ҳ������ָ���ַ����ȥ
-	memcpy(&funptr, (long*)(vptrAddr)+index, 4);//memcpy�ķ����Ǵ������ĵ�ַ��������Ҫ��funptr�ĵ�ַ���룬
-	//��funptr��һ��ָ�룬���Զ�Ӧ��long*)(vptrAddr)+indexҲ��һ��ָ�룬������һ������ָ�룬���������������һ��
-	//1.��longת��long*�൱��ȡ���麯����ָ��ĵ�ַreinterpret_cast<long*>(vptrAddr)��̫ɧ��
-	//2.�ı�ָ��ĵ�ַ���Ըı�ָ���ָ��int* p; int** walk=p; walk
-	(*funptr)(pthis);//funptr(pthis);Ҳ����
+	void(*funptr)(void*);//定义一个函数指针，第一个void是返回类型，第二个void*是该函数的参数表类型
+	long vptrAddr;//保存虚函数表地址
+	memcpy(&vptrAddr, pthis, 4);//把对象的前4个字节也就是虚指针地址传过去
+	memcpy(&funptr, (long*)(vptrAddr)+index, 4);//memcpy的方法是传入对象的地址，这样需要将funptr的地址传入，
+	//而funptr是一个指针，所以对应的long*)(vptrAddr)+index也是一个指针，并且是一个二级指针，这样才能完成类型一致
+	//1.把long转成long*相当于取了虚函数表指针的地址reinterpret_cast<long*>(vptrAddr)可太骚了
+	//2.改变指针的地址可以改变指针的指向int* p; int** walk=p; walk
+	(*funptr)(pthis);//funptr(pthis);也可以
 }
-//ͨ������ָ���޸�һ��ָ��
+//通过二级指针修改一级指针
 /*
 int fun(void)
 {
@@ -80,25 +80,25 @@ int main()
 {
 	cout << call_math(1, 2, add) << endl;
 	//B b;
-	A* arr[2] = { 0 };//���û���ָ���������������
+	A* arr[2] = { 0 };//利用基类指针数组管理派生类
 	arr[0] = new A;
 	arr[1] = new B;
 	arr[0]->test();
 	arr[1]->test();
 	N n;
-	Base a;//a���������һ��_vfptr,_vfptrָ��һ���麯������������ź���ָ�루����ָ��void**����_vfptr[0]��һ������ָ��(void*)��*(_vfptr[0])������ʵ����һ����������
+	Base a;//a对象里多了一个_vfptr,_vfptr指向一个虚函数表，表里存着函数指针（二级指针void**），_vfptr[0]是一个函数指针(void*)，*(_vfptr[0])这样其实就是一个函数名了
 	Child b;
 	Base* p = &b;
-	cout << "sizeof N:" << sizeof N << ",m_aƫ��ֵ" << offsetof(N, m_a) << ",m_bƫ��ֵ" << offsetof(N, m_b) <<endl;//�����Ա��������ƫ��
-	cout << "sizeof Base:" << sizeof Base << ",m_aƫ��ֵ" << offsetof(Base, m_a) << ",m_bƫ��ֵ" << offsetof(Base, m_b) << endl;
-	//virtual������ʵ����һ��ָ�룬�����Ա������˳���޹�
+	cout << "sizeof N:" << sizeof N << ",m_a偏移值" << offsetof(N, m_a) << ",m_b偏移值" << offsetof(N, m_b) <<endl;//计算成员相对于类的偏移
+	cout << "sizeof Base:" << sizeof Base << ",m_a偏移值" << offsetof(Base, m_a) << ",m_b偏移值" << offsetof(Base, m_b) << endl;
+	//virtual函数其实代表一个指针，且与成员的排列顺序无关
 
-	//typedef void(*VFUN)();//VFUN��Ա����ָ�����ͣ�void*����ǰ���ö���ĵ�ַ,��������������Ļ�һ������õ�ַ���þͲ�������&a�����ַ�ˣ������б�
-	typedef void(*VFUN)(void*);//��void*��ʾthisָ�룬��Ϊԭ��û�в�������Ҫtypedef�൱��������һ��ԭ��
-	typedef VFUN* VPTR;//ָ���麯������ָ�����ͣ���һ������ָ��
-	VPTR _vfptr = *(VPTR*)&a;//ָ�������׵�ַ��
-	//VFUN* _vfptr = *(VFUN**)&a;//������ָ��д��
-	//Ҳ����˵һ��ָ��ָ������������ָ��ָ���麯����
+	//typedef void(*VFUN)();//VFUN成员函数指针类型，void*传当前调用对象的地址,但是如果是这样的话一会儿利用地址调用就不用输入&a对象地址了，有利有弊
+	typedef void(*VFUN)(void*);//加void*表示this指针，因为原型没有参数，重要typedef相当于声明了一个原型
+	typedef VFUN* VPTR;//指向虚函数表的指针类型，是一个二级指针
+	VPTR _vfptr = *(VPTR*)&a;//指向对象的首地址，
+	//VFUN* _vfptr = *(VFUN**)&a;//纯二级指针写法
+	//也就是说一级指针指向函数名，二级指针指向虚函数表
 	cout << _vfptr << "[" << _vfptr[0] << "," << _vfptr[1] << "]" << endl;
 	_vfptr[0](&a);
 	_vfptr[1](&a);
@@ -106,8 +106,8 @@ int main()
 	cout << _vfptr1 << "[" << _vfptr1[0] << "," << _vfptr1[1] << "]" << endl;
 	_vfptr1[0](&b);
 	_vfptr1[1](&b);
-	//-----����
-	cout << "----------��һ�ַ���ʹ�ú���call" << endl;//ԭ��Ҳ�Ǻ���ָ��ȡ��������ָ��
+	//-----测试
+	cout << "----------另一种方法使用函数call" << endl;//原理也是函数指针取到虚数表指针
 	CallVirtualFunc(&a, 0);
 	CallVirtualFunc(&a, 1);
 	CallVirtualFunc(&b, 0);
@@ -119,6 +119,6 @@ int main()
 	int** walk = &pp;
 	walk = &mm;
 	//*walk = mm;
-	//--���Խ����ֻ�޸Ķ���ָ�벢�����޸�һ��ָ���ָ����Ҫ*walk�����޸�һ��ָ���ָ��
+	//--测试结果，只修改二级指针并不会修改一级指针的指向，需要*walk才能修改一级指针的指向
 	
 }
