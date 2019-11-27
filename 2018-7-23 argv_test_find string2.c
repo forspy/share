363e9a77f475d487fa86c0dffed4395bc0bb75e5
forspy <40792536+forspy@users.noreply.github.com>
2019-11-27 15:57:19 +0800

    命令行参数argc_argv

--- 2018-7-23 argv_test_find string2.c
+++ 2018-7-23 argv_test_find string2.c
@@ -1,11 +1,11 @@
-
+//注意strstr()函数只在C语言中有效
 #include<stdio.h>
 #include<string.h>
 #define MAXLINE 10000
 
 int getline(char line[], int max);
 
-/*find��������ӡ���һ������ָ����ģʽƥ�����*/
+/*find函数：打印与第一个参数指定的模式匹配的行*/
 main(int argc, int *argv[])
 {
 	char line[MAXLINE];
@@ -26,7 +26,7 @@ main(int argc, int *argv[])
 }
 
 
-/*getline:�������ַ����洢��s�У������������ַ����ĳ���*/
+/*getline:将输入字符串存储在s中，并返回输入字符串的长度*/
 int getline(char s[], int lim)
 {
 	int c, i;
@@ -38,7 +38,7 @@ int getline(char s[], int lim)
 	s[i] = '\0';
 	return i;
 }
-//����cmd�󣬽���·�������룺"argv_test_find string2.exe" a��ע��ֻ��һ�������������ܣ���ӡ�����в��������ַ���
-//����main�������ܱ������������ã� ��˲������ڳ����ڲ�ȡ��ʵ��ֵ����ô���ںδ���ʵ��ֵ����main�������β���? 
-//ʵ����,main�����Ĳ���ֵ�ǴӲ���ϵͳ�������ϻ�õġ�������Ҫ����һ����ִ���ļ�ʱ����DOS��ʾ���¼����ļ�����
-//������ʵ�ʲ������ɰ���Щʵ�δ��͵�main���β���ȥ��
+//进入cmd后，进入路径下输入："argv_test_find string2.exe" a（注意只有一个参数），功能：打印出含有参数名的字符行
+//由于main函数不能被其它函数调用， 因此不可能在程序内部取得实际值。那么，在何处把实参值赋予main函数的形参呢? 
+//实际上,main函数的参数值是从操作系统命令行上获得的。当我们要运行一个可执行文件时，在DOS提示符下键入文件名，
+//再输入实际参数即可把这些实参传送到main的形参中去。
