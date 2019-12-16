2307fb55630fae66e9d8a7524f5edc4e41ff9382
forspy <qiubow@126.com>
2019-12-16 14:37:43 +0800

    测试_getche_getch_getchar的比较

--- 2018-8-1 test_getchar_getch_getche.c
+++ 2018-8-1 test_getchar_getch_getche.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
-//#include<conio.h>
-//
-//int main()
-//{
-//	char ch;
-//	printf("输入一个字符：");
-//	ch = getch();
-//	printf("\n你输入的字符是：'%c'\n", ch);
-//	return 0;
-//}
+//注释：        先CTRL + K + C
+
+//取消注释： 先CTRL + K + U
+#include<conio.h>
+//getch不用按回车，getchar需要按回车
+int main()
+{
+	char ch;
+	printf("输入一个字符：");
+	ch = getch();
+	printf("\n你输入的字符是：'%c'\n", ch);
+	return 0;
+}
 
 //#include<stdio.h>
 //#include<conio.h>
@@ -22,14 +25,14 @@
 //	return 0;
 //}
 
-#include<stdio.h>
-#include<conio.h>
-
-int main()
-{
-	char ch;
-	printf("输入一个字符：");
-	ch = getchar();
-	printf("\n你输入的字符是：'%c'\n", ch);
-	return 0;
-}
+//#include<stdio.h>
+//#include<conio.h>
+//
+//int main()
+//{
+//	char ch;
+//	printf("输入一个字符：");
+//	ch = getchar();
+//	printf("\n你输入的字符是：'%c'\n", ch);
+//	return 0;
+//}
